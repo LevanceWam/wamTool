46e4601fbd5cf7903f963fc94faa7b9d3f08f6a4
LevanceWam <lkwamley@fullsail.edu>
2016-06-21 21:55:49 -0400

    trying to fix wamTool

--- src/util.js
+++ src/util.js
@@ -5,12 +5,12 @@ exports.debug = (title, obj) => {
   colors.setTheme({
     info: 'blue',
     warn: 'yellow',
-    debug: 'red',
-    error: 'green',
+    debug: 'green',
+    error: 'red',
     input: 'orange',
     object: 'cyan',
   });
-  const output = seperator.input + ' ' + title.info + ' ' + obj.info + ' ' + ts.info + ' ' + seperator;
+  const output = colors.debug(seperator) + ' ' + colors.info(title) + ' ' + colors.input(obj) + ' ' + ts.info + ' ' + seperator;
   const fs = require('fs');
   if (process.env.DEBUG) {
     fs.appendFile('logs/util.logs', output, (err) => {
