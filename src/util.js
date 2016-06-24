3a9a5da1e3fd370d96c9dce09c6cfd63d1d2fbba
BHess2653 <GM_Dahaka@BHMac.local>
2016-06-23 23:53:33 -0400

    updated versionBump

--- src/util.js
+++ src/util.js
@@ -1,5 +1,6 @@
+const pkgj = require('../package.json');
 exports.debug = (title, obj) => {
-  const seperator = '\n====================\n';
+  const seperator = '\n====<(°_°<)===(^°_°^)===(>°_°)>====\n';
   const ts = new Date();
   const colors = require('colors');
   colors.setTheme({
@@ -13,9 +14,29 @@ exports.debug = (title, obj) => {
   const output = colors.debug(seperator) + ' ' + colors.info(title) + ' ' + colors.help(obj) + ' ' + colors.warn(ts) + ' ' + colors.debug(seperator);
   const fs = require('fs');
   if (process.env.DEBUG) {
-    fs.appendFile('logs/util.logs', output, (err) => {
-      if (err) throw err;
-      console.log(output);
-    });
+    console.log(output);
   }
 };
+
+exports.versionBump(currentV, semanticV) => {
+  let patch = currentV.patch;
+  let minor = currentV.minor;
+  let major = currentV.major;
+  if (typeof semanticV){
+    if (semanticV === 'patch'){
+      patch += 1;
+      console.log('You have created a Patch');
+    }
+    if (semanticV === 'minor'){
+      minor += 1;
+      console.log('You have created a Minor change');
+    }
+    if (semanticV === 'major'){
+      patch = 0;
+      minor = 0;
+      major += 1;
+      console.warn('You have created a Major change!');
+    }
+  }
+}
+console.log(pkgj.version);
